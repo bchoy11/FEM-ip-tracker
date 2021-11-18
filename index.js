b47d1b0141727cdfbb2e7047ac98c1237acf8287
Brandon Choy <91912156+bchoy11@users.noreply.github.com>
2021-11-18 11:15:47 -0600

    add data request & display

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
+    
+
 function submitSearch(){
     let ip = document.getElementById('ipsearch').value;
-    console.log('ip: '+ip);
     document.getElementById('ipsearch').value='';
     return ip;
 }
 
 function getData(){
-    var ip = submitSearch();
-        var api= "at_6ZjmmDsINfAKhCfGwkFOczalZ22Ml";
+    let ip = submitSearch();
+    let ipAddress = document.getElementById('ip');
+    let location = document.getElementById('location');
+    let timezone = document.getElementById('timezone');
+    let isp = document.getElementById('isp');
+
+        let api= "at_6ZjmmDsINfAKhCfGwkFOczalZ22Ml";
         $(function () {
         $.ajax({
             url: "https://geo.ipify.org/api/v1",
             data: {apiKey: api, ipAddress: ip},
             success: function(data) {
-                let region = JSON.stringify(data.location.region,"",2);
-                let city = JSON.stringify(data.location.city,"",2);
-                let zip = JSON.stringify(data.location.postalCode,"",2);
-                $("body").append("<pre>"+ JSON.stringify(data.location,"",2)+"</pre>");
-                $("body").append("<pre>ip: "+ JSON.stringify(data.ip,"",2)+"</pre>");
-                $("body").append("<pre>location: "+ city+', '+region+' '+zip+"</pre>");
-                $("body").append("<pre>city: "+ city+"</pre>");
-                $("body").append("<pre>zip: "+ zip+"</pre>");
+                let region = data.location.region;
+                let city = data.location.city;
+                let zip = data.location.postalCode;
+                let zone = data.location.timezone;
+                let servicer = data.isp;
+                ipAddress.append(data.ip);
+                location.append(city+', '+region+' '+zip);
+                timezone.append('UTC '+zone);
+                isp.append(servicer);
             }
         });
         });
