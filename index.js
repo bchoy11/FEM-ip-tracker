24ce77e5274cbbfe6895a377a7a96c9c42468bb7
Brandon Choy <91912156+bchoy11@users.noreply.github.com>
2021-11-22 11:16:34 -0600

    added map API

--- index.js
+++ index.js
@@ -32,3 +32,10 @@ function getData(){
         });
         });
 }
+
+const mymap = L.map('map').setView([51.505, -0.09], 13);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileURL, {attribution});
+
+tiles.addTo(mymap);
